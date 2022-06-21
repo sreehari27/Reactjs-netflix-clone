eac35b36b935a4dc8d0e4d11749e9c2d8123be00
sreehari <sreehari27jan@gmail.com>
2022-06-21 20:59:14 +0530

    UI design completed

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='navebar'>
+      <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158'/>
+      <img className='avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'/>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './RowPost.css'
+function RowPost() {
+    return (
+        <div className='row'>
+            <h2>Title</h2>
+            <div className="posters">
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+                <img className='poster' src="https://images-na.ssl-images-amazon.com/images/I/71w8USjAGfL._RI_.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default RowPost
